b309e8daa74d8701fb1e66f5dcc97d2df71a712f
Olivier <opaquien@wiremind.io>
2023-06-29 16:38:58 +0200

    fix: Simplify dind :rocket:

--- docker/dind/entrypoint.sh
+++ docker/dind/entrypoint.sh
@@ -2,22 +2,8 @@
 
 set -em
 
-# export DOCKER_HOST="unix:///run/user/1000/docker.sock"
-
 USER_ID=$(stat -c '%u' /app)
 GROUP_ID=$(stat -c '%g' /app)
-
-echo "Starting docker daemon..."
-
-dockerd-entrypoint.sh dockerd > /var/log/dockerd.log 2>&1 &
-
-while ! docker ps
-do
-    echo "Waiting for docker daemon..."
-    sleep 1
-done
-
-echo "Docker daemon started"
 
 echo "Launching gitlab-ci-local $@"
 
@@ -33,7 +19,5 @@ echo "Fixing folder permissions to $USER_ID:$GROUP_ID"
 
 chown -R $USER_ID:$GROUP_ID /app
 
-if [[ "$STATUS" != "0" ]]
-then
-    exit $STATUS
-fi
+echo "exit $STATUS"
+exit $STATUS
